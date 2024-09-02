019f63e0ae33cd389b6f3a528fa03cfb7989734c
mizuki0220 <em9220me.i@gmail.com>
2024-09-02 15:54:28 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def mypage
-    @user = current_user
-    @posts = Post.all
+    @posts = current_user.posts
+    @post_genres = PostGenre.all
   end
 
   def edit
@@ -18,4 +20,18 @@ class UsersController < ApplicationController
 
   def withdraw
   end
+  
+    private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to user_path(current_user.id)
+    end
+  end
+
 end
