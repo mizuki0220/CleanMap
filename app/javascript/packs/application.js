1ff8f99bde016181b0b37299691c2b5187183e25
mizuki0220 <em9220me.i@gmail.com>
2024-09-23 12:27:07 +0000

    1stデプロイ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,6 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application";
-import "../stylesheets/map";
 import "../stylesheets/top";
 import "../stylesheets/index";
 import "../stylesheets/show";
