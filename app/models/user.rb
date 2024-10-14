2cf91e7f41de84a5a58e4166bd129584de2669a4
mizuki0220 <em9220me.i@gmail.com>
2024-10-14 16:13:32 +0000

    comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
-  has_many :comment, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
 
