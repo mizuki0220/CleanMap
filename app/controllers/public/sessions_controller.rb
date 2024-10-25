45c7149107de806df46bdcde82edf37187910add
mizuki0220 <em9220me.i@gmail.com>
2024-10-25 16:11:49 +0000

    admin

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -51,12 +51,13 @@ class Public::SessionsController < Devise::SessionsController
   def reject_user
     @user = User.find_by(email: params[:user][:email].downcase)
     if @user
-      if (@user.valid_password?(params[:email][:password]) && (@user.active_for_authentication? == false))
+      if (@user.valid_password?(params[:user][:password]) && (@user.active_for_authentication? == false))
         flash[:error] = "退会済みです。再度ご登録をしてご利用ください"
         redirect_to new_user_registration_path
       end
     else
         flash[:error] = "項目を入力してください"
+        redirect_to new_user_registration_path
     end
   end
 
