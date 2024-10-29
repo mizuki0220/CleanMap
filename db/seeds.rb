b7ed8aec4ed3f1144f148be87aa1d119c0150823
mizuki0220 <em9220me.i@gmail.com>
2024-10-29 12:40:08 +0000

    admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Admin.find_or_create_by!(email: "admin@example.com") do |admin|
+  admin.password = "password"
+end
+
 あいちゃん = User.find_or_create_by!(email: "aiai@example.com") do |user|
   user.name = "あいちゃん"
   user.password = "password"
